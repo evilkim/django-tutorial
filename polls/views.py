07314908085eda6178d8e1e49172680be2fb4c6e
Kim Eric Taylor <ekimtaylor@gmail.com>
2017-12-14 19:41:36 -0500

    a-shortcut-render, raising-a-404-error, use-the-template-system, removing-hardcoded-urls-in-templates

--- polls/views.py
+++ polls/views.py
@@ -2,18 +2,29 @@
 
 from .models import Question
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
+# from django.template import loader
 
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = "Hello, world. You're at the polls index.<br/>"
-    output += ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    context = { 'latest_question_list': latest_question_list }
+    # template = loader.get_template('polls/index.html')
+    # return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    """ low-level equivalent:
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
